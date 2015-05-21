b2ca695829a503ec68763430e4296506df2a7886
Antonio Lima <anto87@gmail.com>
2015-05-21 16:30:19 -0400

    Move module

--- setup.py
+++ setup.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(name='dtw',
-      version='1.0',
-      description='Python DTW Module',
-      author='Pierre Rouanet',
-      author_email='pierre.rouanet@gmail.com',
-      url='https://github.com/pierre-rouanet/dtw',
-      license='GNU GENERAL PUBLIC LICENSE Version 3',
-
-      install_requires=['numpy'],
-      setup_requires=['setuptools_git >= 0.3', ],
-
-      py_modules=['dtw'],
-      )
+    version='1.0',
+    description='Python DTW Module',
+    author='Pierre Rouanet',
+    author_email='pierre.rouanet@gmail.com',
+    url='https://github.com/pierre-rouanet/dtw',
+    license='GNU GENERAL PUBLIC LICENSE Version 3',
+    install_requires=['numpy'],
+    packages=find_packages(),
+)
